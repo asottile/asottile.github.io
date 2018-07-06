b4341da1f6d75a3d5d1abae513f066e9357efc47
Anthony Sottile <asottile@umich.edu>
2018-07-05 17:42:20 -0700

    Fix flake8 issues

--- get-virtualenv.py
+++ get-virtualenv.py
@@ -19,8 +19,8 @@ else:
     from urllib.request import urlopen
 
 
-TGZ = 'https://files.pythonhosted.org/packages/33/bc/fa0b5347139cd9564f0d44ebd2b147ac97c36b2403943dbee8a25fd74012/virtualenv-16.0.0.tar.gz'
-EXPECTED_SHA256 = 'ca07b4c0b54e14a91af9f34d0919790b016923d157afda5efdde55c96718f752'
+TGZ = 'https://files.pythonhosted.org/packages/33/bc/fa0b5347139cd9564f0d44ebd2b147ac97c36b2403943dbee8a25fd74012/virtualenv-16.0.0.tar.gz'  # noqa
+EXPECTED_SHA256 = 'ca07b4c0b54e14a91af9f34d0919790b016923d157afda5efdde55c96718f752'  # noqa
 PKG_PATH = '.virtualenv-pkg'
 
 
